cd94810ffc943223ef31aa45bec3256415bbae90
thethang12-git <thethang.18.12@gmail.com>
2025-06-16 19:58:42 +0700

    bai luyen tap

---
+++ js/baitoanguitietkiem.js
@@ -0,0 +1,13 @@
+function tinhToan() {
+    var sotien = document.getElementById("sotien").value;
+    var sothang = document.getElementById("sothang").value;
+    var laisuat = 0.075
+    var laisuatthang = laisuat / 12; 
+
+    sotien = parseFloat(sotien);
+    sothang = parseInt(sothang);
+
+    var tongSoTien = sotien * laisuatthang * sothang + sotien;
+
+    alert("Số tiền bạn nhận được là: " + tongSoTien + " đồng");
+}
