dfd4812c4cc6e3861780cb5456aa0a7f6deb38b4
thethang12-git <thethang.18.12@gmail.com>
2025-06-17 09:37:40 +0700

    bai toan gui tiet kiem

--- js/baitoanguitietkiem.js
+++ js/baitoanguitietkiem.js
@@ -3,11 +3,8 @@ function tinhToan() {
     var sothang = document.getElementById("sothang").value;
     var laisuat = 0.075
     var laisuatthang = laisuat / 12; 
-
     sotien = parseFloat(sotien);
     sothang = parseInt(sothang);
-
     var tongSoTien = sotien * laisuatthang * sothang + sotien;
-
     alert("Số tiền bạn nhận được là: " + tongSoTien + " đồng");
 }
