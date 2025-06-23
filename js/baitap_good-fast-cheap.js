c8573e604cbb96d09aa2ba28fca2a56437d12c19
thethang12-git <thethang.18.12@gmail.com>
2025-06-23 17:27:54 +0700

    can chua bai toan good fast cheap

--- js/baitap_good-fast-cheap.js
+++ js/baitap_good-fast-cheap.js
@@ -2,23 +2,37 @@
 // good = true -  cheap = true - fast = false
 // good = true - cheap = false - fast = true
 // good = false - cheap = true - fast = true
-var GOOD = document.getElementById('GOOD')
-var CHEAP = document.getElementById('CHEAP')
-var FAST = document.getElementById('FAST')
+// 
 function check(){
-if (GOOD.checked && CHEAP.checked){
-    FAST.checked = false
-    console.log('case 1')
-}
-else if (GOOD.checked && FAST.checked  ){
-    CHEAP.checked = false
-    console.log('case 2')
-}
-else if (CHEAP.checked && FAST.checked) {
-    GOOD.checked = false
-    console.log('case 3')
-}
-else {
-    console.log('case 4')
-}
-}
+   
+    var GOOD = document.getElementById('GOOD');
+    var CHEAP = document.getElementById('CHEAP');
+    var FAST = document.getElementById('FAST');
+
+    var isGood = GOOD.checked;
+    var isCheap = CHEAP.checked;
+    var isFast = FAST.checked;
+
+    if (isGood && isCheap){
+        if (isFast){
+            GOOD.checked = false; 
+            console.log('case 0');
+        }
+        else if (!isFast && !isGood){ {
+            FAST.checked = false; 
+            console.log('case 1');
+        }
+        if (isFast && !isGood){
+            CHEAP.checked = false; 
+            console.log('case 4');
+        }
+    }
+    else if (isGood && isFast){
+        CHEAP.checked = false;
+        console.log('case 2');
+    }
+    else if (isCheap && isFast){
+        GOOD.checked = false;
+        console.log('case 3');
+    }
+}}
