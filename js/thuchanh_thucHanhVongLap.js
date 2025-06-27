80a5d185ea73b469b6ecfe73b64e37fbf6247451
the thang <thethang.18.12@gmail.com>
2025-06-27 11:31:50 +0700

    done vong lap 1

--- js/thuchanh_thucHanhVongLap.js
+++ js/thuchanh_thucHanhVongLap.js
@@ -25,10 +25,64 @@ function bai3(){
     let b = 1 ;
     let num = 0;
     for (let i =0; i < 20; i++) {
+        document.write(a)
         num = a + b
-        a = num - b 
-        b = num; // 1 2
-        document.write(num + '<br>')
-        console.log(a +',' + b)
+        a = b
+        b = num
     }
+}
+
+//bài 4
+function bai4(){
+    let a = 0;
+    let b = 1 ;
+    let num = 0;
+    for (let i =0; i < 20; i++) {
+        num = a + b
+        a = b
+        b = num
+        if(a % 5 == 0){document.write('số đầu tiên chia hết cho 5 là' + ' ' +a) ; break}
+    }
+}
+
+//bài 5
+let total = 0
+function bai5(){
+    let a = 0;
+    let b = 1 ;
+    let num = 0;
+    for (let i =0; i < 20; i++) {
+        total += a
+        num = a + b
+        a = b
+        b = num
+    }
+    document.write( `tổng của 20 số đầu trong dãy fibonacy là ${total}`)
+}
+
+// bài 6
+function bai6(){
+    let x = 0;
+    let temp = 0;
+    while (x < 30) {
+        for(let i = temp;i < 999; i++ ){
+            if (i % 7 == 0){ temp = i + 1 ; document.write(i + ' ');  break }
+        } 
+        x++
+    }
+}
+
+// bài 7
+function bai7(){
+    let x = 0
+    do{
+        if(x % 3 ==0 && x % 5 == 0){
+            document.write('FizzBuzz' + '<br>')
+        }
+        else if (x % 3 == 0) { document.write('Fizz' + '<br>')}
+        else if ( x % 5 == 0) {document.write('Buzz' + '<br>')}
+        else (document.write(x + '<br>'))
+        x++
+    }
+    while (x <= 100)
 }
