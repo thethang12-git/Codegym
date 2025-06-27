52304e265be1c4b52a61430c537c2ab166c0293c
thethang12-git <thethang.18.12@gmail.com>
2025-06-27 14:50:36 +0700

    xem lai thuchanh_vongLap2

--- js/thuchanh_vongLap2.js
+++ js/thuchanh_vongLap2.js
@@ -1,4 +1,30 @@
-function bai1() {
+// Bài 1
+function bai1(){
+    let a = 0;
+    let b = 1 ;
+    let num = 0;
+    for (let i =0; i < 20; i++) {
+        document.write(a + '<br>')
+        num = a + b
+        a = b
+        b = num
+    }
+}
+// bai 2
+// tính giai thừa của số nguyên dương 5!= 5 * 4 * 3 * 2 * 1
+function bai2(){
+    let result = 1;
+    let num = +prompt('nhập vào số nguyên dương')
+    while (num <= 0 || isNaN(num)){
+            num = +prompt('nhập lại số') 
+        }
+    for (let i = num; i > 0; i--){ 
+        result = result * i 
+        }
+    document.write(`giai thừa của ${num} là ${result}`);    
+}
+// bài 3
+function bai3() {
     for (let i = 1; i <= 5; i++) {
          let sao = '';
         for (let j = 1; j <= i; j++) {
@@ -26,3 +52,4 @@ function bai1() {
         document.write(space + sao3 + '<br>');
     }
 }
+
