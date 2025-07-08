68ea97947af9049c0b0c6ba7fd63ad846c7b3793
the thang <thethang.18.12@gmail.com>
2025-07-08 12:32:22 +0700

    done

---
+++ js/baitap_quanLySanPham.js
@@ -0,0 +1,28 @@
+console.log('hé hé')
+let product = []
+let display = document.getElementById('display')
+let inputt = document.getElementById('input')
+function add(){
+    product.push(inputt.value)
+    display.innerHTML = ''
+    for(let x = 0;x < product.length;x++){
+    display.innerHTML += `<p> ${product[x]}  <button onclick='edit(${x})'> edit </button> <button onclick='deletee(${x})'> delete </button> </p> <br>`
+}
+    inputt.value = ''
+}
+
+function edit(event){
+    console.log(product[event])
+    product[event] = prompt('sửa giá trị mới')
+    display.innerHTML = ''
+    for(let x = 0;x < product.length;x++){
+    display.innerHTML += `<p> ${product[x]}  <button onclick='edit(${x})'> edit </button> <button onclick='deletee(${x})'> delete </button> </p> <br>`
+}
+}
+function deletee(event){
+    product.splice(event, 1)
+    display.innerHTML = ''
+    for(let x = 0;x < product.length;x++){
+    display.innerHTML += `<p> ${product[x]}  <button onclick='edit(${x})'> edit </button> <button onclick='deletee(${x})'> delete </button> </p> <br>`
+}
+}
