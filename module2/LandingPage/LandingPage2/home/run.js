e9998bca30791b08b62f32b5d83e8debdeb04e68
thethang <thethang.18.12@gmail.com>
2025-08-20 15:48:07 +0700

    added

--- module2/LandingPage/LandingPage2/home/run.js
+++ module2/LandingPage/LandingPage2/home/run.js
@@ -16,3 +16,150 @@ function onFocus(event){
 }
 }
 
+
+function colorChange(event,color){
+    let item = event.target;
+    item.style.color = color;
+    if(event.target.classList.contains('fa-regular')){
+        item.classList.remove('fa-regular')
+        item.classList.add('fa-solid');
+        item.style.color = '#EF6820'
+    }
+    else if (event.target.classList.contains('fa-solid')) {
+        item.classList.remove('fa-solid')
+        item.classList.add('fa-regular');
+        item.style.color = 'black'
+    }
+    if(event.target.getAttribute('src') === '../asset/Radio.png'){
+        event.target.setAttribute('src','../asset/Radio2.png')
+        event.target.style.width = '20px'
+        event.target.style.height = '20px'
+        event.target.style.marginLeft = '8px'
+    }
+    else if ((event.target.getAttribute('src') === '../asset/Radio2.png')) {
+        event.target.setAttribute('src','../asset/Radio.png')
+        event.target.style.width = '28px'
+        event.target.style.height = '28px'
+        event.target.style.marginLeft = '0'
+    }
+}
+
+let collapsed = false;
+
+function toggleSidebar() {
+    let item = document.querySelector('.navbar');
+    if (!collapsed) {
+        item.style.width = '0px';
+        item.style.overflow = 'hidden';
+        item.style.padding = '8px 0';
+    } else {
+        item.style.width = '237px';
+        item.style.padding = '8px 16px';
+    }
+    collapsed = !collapsed;
+}
+
+function hehehe(idd,event){
+    let item = document.getElementById(idd);
+    let currentColor = getComputedStyle(item).color;
+    if(currentColor === 'rgb(157, 164, 174)'){
+        item.style.color = '#F04438'
+    }
+    else if (currentColor === 'rgb(240, 68, 56)') {
+        item.style.color = '#9DA4AE'
+    }
+    console.log(event.target.style.color)
+    let currentColor2 = getComputedStyle(event.target).color;
+    if (currentColor2 === 'rgb(157, 164, 174)'){
+        event.target.style.color = '#F04438'
+    }
+    else if (currentColor2 === 'rgb(240, 68, 56)') {
+        event.target.style.color = '#9DA4AE'
+    }
+}
+
+// function dislayModify(){
+//     let item = document.querySelector('.content-body');
+//     let item1 = document.querySelector('.content-body--1');
+//     let item2 = document.querySelector('.content-body--2');
+//     let item3 = document.querySelector('.content-body--3');
+//     item1.style.width = '29.2rem'
+//     item2.style.width = '28.3rem'
+//     item3.style.flex = '1'
+//     setTimeout(() => {
+//         item.style.flexDirection = 'row'
+//             }
+//         , 500)
+// }
+// function display(){
+//     let item = document.querySelector('.content-body');
+//     let item1 = document.querySelector('.content-body--1');
+//     let item2 = document.querySelector('.content-body--2');
+//     let item3 = document.querySelector('.content-body--3');
+//     item3.style.width = '15rem'
+//     setTimeout(() => {
+//             item1.style.width = '100%'
+//         }
+//         , 100)
+//     setTimeout(() => {
+//             item2.style.width = '100%'
+//         }
+//         , 400)
+//     setTimeout(() => {
+//             item3.style.width = '100%'
+//         }
+//         , 600)
+//     setTimeout(() => {
+//             item.style.flexDirection = 'column'
+//         }
+//         , 300)
+// }
+
+
+function displayMenu() {
+    let item = document.querySelector('.content-body');
+    let item1 = document.querySelector('.content-body--1');
+    let item2 = document.querySelector('.content-body--2');
+    let item3 = document.querySelector('.content-body--3');
+    let button = document.querySelector('.toggleDisplay');
+    let button_left = document.querySelector('.toggle1');
+    let button_right = document.querySelector('.toggle2');
+    if(button.getAttribute('data-status')=== 'left') {
+        button.setAttribute('data-status', 'right');
+        button_right.style.borderRadius = '55px';
+        button_right.style.background = 'white';
+        button_left.style.background = '#E5E7EB';
+        //
+        item1.style.width = '29.2rem'
+        item2.style.width = '28.3rem'
+        item3.style.flex = '1'
+        setTimeout(() => {
+                item.style.flexDirection = 'row'
+            }
+            , 500)
+    }
+    else {
+        button.setAttribute('data-status', 'left');
+        button_left.style.borderRadius = '55px';
+        button_left.style.background = 'white';
+        button_right.style.background = '#E5E7EB';
+        //
+        item3.style.width = '15rem'
+        setTimeout(() => {
+                item1.style.width = '100%'
+            }
+            , 100)
+        setTimeout(() => {
+                item2.style.width = '100%'
+            }
+            , 400)
+        setTimeout(() => {
+                item3.style.width = '100%'
+            }
+            , 600)
+        setTimeout(() => {
+                item.style.flexDirection = 'column'
+            }
+            , 300)
+    }
+}
